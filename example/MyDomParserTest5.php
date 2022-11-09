f520eaab126f85638d5d0debdde010d4eeeebb8c
rockyakisoba <mmtrokuroku96@gmail.com>
2022-11-10 02:44:00 +0900

    exampleのini_set()を削除

--- example/MyDomParserTest5.php
+++ example/MyDomParserTest5.php
@@ -1,11 +1,5 @@
 <?php
 
-date_default_timezone_set("Asia/Tokyo");
-ini_set("arg_separator.output", "&");
-ini_set('xdebug.var_display_max_children', -1);
-ini_set('xdebug.var_display_max_data', -1);
-ini_set('xdebug.var_display_max_depth', -1);
-
 require_once __DIR__ . "/../vendor/autoload.php";
 
 use Rocketsoba\DomParserWrapper\DomParserAdapter;
--- example/OrignalDomParserTest2.php
+++ example/OrignalDomParserTest2.php
@@ -1,14 +1,8 @@
 <?php
 
-date_default_timezone_set("Asia/Tokyo");
-ini_set("arg_separator.output", "&");
-ini_set('xdebug.var_display_max_children', -1);
-ini_set('xdebug.var_display_max_data', -1);
-ini_set('xdebug.var_display_max_depth', -1);
-
 require_once __DIR__ . "/../vendor/autoload.php";
 
-use \KubAT\PhpSimple\HtmlDomParser;
+use KubAT\PhpSimple\HtmlDomParser;
 
 $dom = HtmlDomParser::str_get_html("
 <html>
